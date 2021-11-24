09f89d2e2addd42f4872bf639dd35c96fcf83f71
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-24 16:47:45 +0100

    implement kernel

--- classification.py
+++ classification.py
@@ -1,16 +1,17 @@
 import numpy as np
+from typing import Callable
 
-class Classifiers:
+class LDA:
 
-    @classmethod
-    def LDA (cls, data):
+    def __init__ (self, data):
         """
         Linear discriminant analysis (LDA)
         Learn normal distribution from training data
         """
         
         # Calculate covariance matrix from training data
-        sigma = np.cov (data[:, :-1].T)
+        self.sigma = np.cov (data[:, :-1].T)
+        self.sigma_inv = np.linalg.inv (self.sigma)
 
         # Extract 0 and 1 classes
         mask_0 = (data[:, -1] == 0)
@@ -19,126 +20,161 @@ class Classifiers:
         data_1 = data[mask_1, :-1]
 
         # Calculate averages from training data
-        mu_0 = np.average(data_0, axis=0)
-        mu_1 = np.average(data_1, axis=0)
-
-        # LDA condition
-        w = np.dot ( np.linalg.inv(sigma), mu_1 - mu_0 )
-        c = np.dot ( w, 0.5 * (mu_1 + mu_0) )
-
-        # Create classifier function (-> {0, 1})
-        def classifier (x):
-            return int(np.dot(w, x) > c)
-
-        return classifier
-
-
-    @classmethod
-    def QDA_wikipedia (cls, data):
-        """
-        Quadratic discriminant analysis (QDA)
-        """
-
-        # Extract 0 and 1 classes
-        mask_0 = (data[:, -1] == 0)
-        data_0 = data[mask_0, :-1]
-        mask_1 = (data[:, -1] == 1)
-        data_1 = data[mask_1, :-1]
-
-        # Calculate covariance matrices from training data
-        sigma_0 = np.cov (data_0.T)
-        sigma_1 = np.cov (data_1.T)
-
-        # Calculate averages from training data
-        mu_0 = np.average(data_0, axis=0)
-        mu_1 = np.average(data_1, axis=0)
-
-        # Create classifier function (-> {0, 1})
-        def classifier (x, threshold=0):
-
-            formula = [
-                np.dot(
-                    np.dot(
-                        (x - mu_0).T,
-                        np.linalg.inv(sigma_0)
-                    ),
-                    (x - mu_0)
-                ),
-                np.log(np.linalg.det(sigma_0)),
-                -1 * np.dot(
-                    np.dot(
-                        (x - mu_1).T,
-                        np.linalg.inv(sigma_1)
-                    ),
-                    (x - mu_1)
-                ),
-
-                -1 * np.log(np.linalg.det(sigma_1)),
-            ]
-
-            return (np.sum(formula) > threshold)
-
-        return classifier
-
-    @classmethod
-    def QDA (cls, data):
-        """
-        Quadratic discriminant analysis (QDA)
-        """
-
-        # Extract 0 and 1 classes
-        mask_0 = (data[:, -1] == 0)
-        data_0 = data[mask_0, :-1]
-        mask_1 = (data[:, -1] == 1)
-        data_1 = data[mask_1, :-1]
-
-        # Calculate covariance matrices from training data
-        sigma_0 = np.cov (data_0.T)
-        sigma_1 = np.cov (data_1.T)
-
-        # Calculate averages from training data
-        mu_0 = np.average(data_0, axis=0)
-        mu_1 = np.average(data_1, axis=0)
-
+        self.mu_0 = np.average(data_0, axis=0)
+        self.mu_1 = np.average(data_1, axis=0)
 
         # Calculate estimated densities
-        pi_0 = data_0.shape[0]
-        pi_1 = data_1.shape[0]
+        self.pi_0 = data_0.shape[0]
+        self.pi_1 = data_1.shape[0]
+
+        # LDA condition
+        self.w = np.dot ( self.sigma_inv, self.mu_1 - self.mu_0 )
+        self.c = np.dot ( self.w, 0.5 * (self.mu_1 + self.mu_0) )
 
 
-        a_0 = -0.5 * (np.log(np.linalg.det(sigma_0)))
-        a_1 = -0.5 * (np.log(np.linalg.det(sigma_1)))
+        self.k_01 = np.dot (
+            np.dot (
+                self.mu_0.T,
+                self.sigma_inv
+            ),
+            self.mu_0
+        )
+        self.k_11 = np.dot (
+            np.dot (
+                self.mu_1.T,
+                self.sigma_inv
+            ),
+            self.mu_1
+        )
+
+    def predict (self, x):
+        # Create classifier function (-> {0, 1})
+        return int(np.dot(self.w, x) > self.c)
+
+    def predict_seb (self, x):
+
+        k_00 = -2 * np.dot (
+            np.dot (
+                self.mu_0.T,
+                self.sigma_inv
+            ),
+            x
+        )
+
+        k_10 = -2 * np.dot (
+            np.dot (
+                self.mu_1.T,
+                self.sigma_inv
+            ),
+            x
+        )
+        
+        k_0 = k_00 + self.k_01 - 2 * np.log (self.pi_0)
+        k_1 = k_10 + self.k_11 - 2 * np.log (self.pi_1)
+
+        # Calculate argmin
+        if k_0 > k_1: return 1
+        else: return 0
 
 
-        def classifier (x):
-
-            b_0 = np.dot (
-                np.dot (
-                    (x - mu_0).T,
-                    np.linalg.inv(sigma_0)
-                ),
-                x - mu_0
-            )
-            delta_0 = a_0 + b_0 + np.log(pi_0)
-
-            b_1 = np.dot (
-                np.dot (
-                    (x - mu_1).T,
-                    np.linalg.inv(sigma_1)
-                ),
-                x - mu_1
-            )
-            delta_1 = a_1 + b_1 + np.log(pi_1)
-
-            return int(delta_1 > delta_0)
-
-        return classifier
 
 
 
 class QDA:
 
-    def __init__ (self):
+    def __init__ (self, data):
+        """
+        Quadratic discriminant analysis (QDA)
+        """
+
+        
+        # Extract 0 and 1 classes
+        mask_0 = (data[:, -1] == 0)
+        data_0 = data[mask_0, :-1]
+        mask_1 = (data[:, -1] == 1)
+        data_1 = data[mask_1, :-1]
+
+        # Calculate covariance matrices from training data
+        self.sigma_0 = np.cov (data_0.T)
+        self.sigma_1 = np.cov (data_1.T)
+
+        # Calculate averages from training data
+        self.mu_0 = np.average(data_0, axis=0)
+        self.mu_1 = np.average(data_1, axis=0)
+
+
+        # Calculate estimated densities
+        self.pi_0 = data_0.shape[0]
+        self.pi_1 = data_1.shape[0]
+
+
+        self.a_0 = -0.5 * (np.log(np.linalg.det(self.sigma_0)))
+        self.a_1 = -0.5 * (np.log(np.linalg.det(self.sigma_1)))
+
+
+    def predict (self, x):
+
+        b_0 = -0.5 * np.dot (
+            np.dot (
+                (x - self.mu_0).T,
+                np.linalg.inv(self.sigma_0)
+            ),
+            x - self.mu_0
+        )
+        delta_0 = self.a_0 + b_0 + np.log(self.pi_0)
+
+        b_1 = -0.5 * np.dot (
+            np.dot (
+                (x - self.mu_1).T,
+                np.linalg.inv(self.sigma_1)
+            ),
+            x - self.mu_1
+        )
+        delta_1 = self.a_1 + b_1 + np.log(self.pi_1)
+
+        return int(delta_1 > delta_0)
+
+
+
+
+class Kernel:
+
+    def __init__ (self, train_data, func: Callable):
+        self.train_data = train_data
+        self.func = func
+
+        # Extract 0 and 1 classes
+        mask_0 = (train_data[:, -1] == 0)
+        self.data_0 = train_data[mask_0, :-1]
+        mask_1 = (train_data[:, -1] == 1)
+        self.data_1 = train_data[mask_1, :-1]
+
+
+    def predict (self, x):
+        x_arr = np.ones(self.train_data.shape[0])
+        x_arr = np.column_stack((x_arr * x[0], x_arr * x[1], x_arr * x[2]))
+        a_0 = np.sum (self.func (x_arr, self.train_data[:, :-1]))
+
+        x_arr_0 = np.ones(self.data_0.shape[0])
+        x_arr_0 = np.column_stack((x_arr_0 * x[0], x_arr_0 * x[1], x_arr_0 * x[2]))
+        x_arr_1 = np.ones(self.data_1.shape[0])
+        x_arr_1 = np.column_stack((x_arr_1 * x[0], x_arr_1 * x[1], x_arr_1 * x[2]))
+
+        s_0 = np.sum (self.func(x_arr_0, self.data_0))
+        s_1 = np.sum (self.func(x_arr_1, self.data_1))
+
+        p_0 = s_0 / a_0
+        p_1 = s_1 / a_0
+
+        if p_1 >= p_0: return 1
+        else: return 0
+
+
+
+class LogisticRegression:
+
+    def __init__ (self, train_data: np.ndarray):
         pass
 
-    
+    def predict (self, x):
+        pass
--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 import os
 
-from classification import Classifiers
+from classification import LDA, QDA, Kernel
 from scoring import Metrics
 
 
@@ -16,8 +16,8 @@ DIRPATH = 'src'
 
 import_path = os.path.join(DIRPATH, 'export.npy')
 data = np.load (import_path)
+#data[:, :-1] = data[:, :-1]/255.
 vals_nb = data.shape[0]
-# print(data)
 
 
 
@@ -40,21 +40,8 @@ vals_nb = data.shape[0]
 
 
 
-#class Data:
 
-#     def __init__ (self, values, labels):
-#         self.values = values
-#         self.labels = labels
-#         pass
-
-
-
-
-
-
-
-
-# Create TRAIN and TEST sets
+########## Create TRAIN and TEST sets
 def create_sets(data, train_frac=0.05):
     # Create (train, test)
     data_shuffled = data.copy()
@@ -63,32 +50,49 @@ def create_sets(data, train_frac=0.05):
     # print(f'training on {train_nb} vals')
     return data_shuffled[:train_nb], data_shuffled[train_nb:]
 
-data_train, data_test = create_sets(data)
+data_train, data_test = create_sets(data, train_frac=0.3)
 
 
 ########## LDA f1=0.8875528357524567
 # Train LDA
-# lda = Classifiers.LDA (data_train)
+# lda = LDA (data_train)
 # # Test LDA
-# pred_lda = np.array([lda(x) for x in data_test[:, :-1]])
+# pred_lda = np.array([lda.predict_seb(x) for x in data_test[:, :-1]])
 # metrics_lda = Metrics(data_test[:, -1], pred_lda)
 # print(metrics_lda.f_score())
 
 
-########## QDA f1=
+########## QDA f1=0.9016371262346097
 # Train QDA
-qda = Classifiers.QDA (data_train)
-print('QDA trained')
-# Test QDA
-pred_qda = np.array([qda(x) for x in data_test[:10000, :-1]])
-metrics_qda = Metrics(data_test[:10000, -1], pred_qda)
-print(metrics_qda.f_score())
+# qda = QDA (data_train)
+# # Test QDA
+# pred_qda = np.array([qda.predict(x) for x in data_test[:10000, :-1]])
+# metrics_qda = Metrics(data_test[:10000, -1], pred_qda)
+# print(metrics_qda.f_score())
 
 
+########## KERNEL f1=0.8175104862054705
+def func (x1: np.ndarray, x2: np.ndarray):
+    dist = np.linalg.norm(x1 - x2)
+    if dist > 1: return 1 / np.linalg.norm(x1 - x2)
+    else: return 1
+    # return np.dot(x1, x2.T)
+
+kernel = Kernel (data_train[:100000], func=func)
+
+# pred_kernel_list = list()
+# data_test_nb = data_test[:100000].shape[0]
+# for i, x in enumerate(data_test[:100000, :-1]):
+#     pred_kernel_list .append(kernel.predict(x))
+#     print(f'Progress: {round(100*i/data_test_nb, 6)}%')
+# pred_kernel = np.array(pred_kernel_list)
+pred_kernel = np.array([kernel.predict(x) for x in data_test[:100000, :-1]])
+metrics_kernel = Metrics(data_test[:100000, -1], pred_kernel)
+print(metrics_kernel)
+print(metrics_kernel.f_score())
 
 
-#qda = Classifiers.QDA (data)
-
+########## REGRESSION
 
 
 
